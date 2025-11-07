cf45a1250f063fcab803a28cdb0ee2ccb641b8d5
JuanMero2002 <154101430+JuanMero2002@users.noreply.github.com>
2025-11-07 12:24:16 -0500

    Update migrate_to_supabase.py

--- migrate_to_supabase.py
+++ migrate_to_supabase.py
@@ -12,7 +12,7 @@ import sys
 import django
 import json
 
-# Configurar Django
+# Configurar Djangoo
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_practicas.settings')
 
